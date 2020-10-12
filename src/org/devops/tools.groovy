5ccbf82d3607633c9fbf34e8bf97e0b0574e9ad8
Joe <wennjoe@163.com>
2020-10-12 16:28:29 +0800

    add color param

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,7 +1,7 @@
 package org.devops
 
 // 打印content内容
-def printmsg(value){
+def printmsg(value,color){
   colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
               'blue'  : "\033[47;34m ${value} \033[0m",
               'green' : "[1;32m>>>>>>>>>>${value}>>>>>>>>>>[m",
