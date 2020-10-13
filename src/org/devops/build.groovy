486a5645fd4e18474c6a8bd79ef803e65d695d0f
Joe <wennjoe@163.com>
2020-10-13 17:03:26 +0800

    encapsulation librarylibrary

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -3,7 +3,7 @@ package org.devops
 def Build(buildtype, buildcommand){
   def buildtools = ["mvn":"M3","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
 
-  println(${buildtype})
+  println("当前选择的构建类型为: ${buildtype}")
   buildhome = tool buildtools[buildtype]
   sh "${buildhome}/bin/${buildtype} ${buildcommand}"
 }
